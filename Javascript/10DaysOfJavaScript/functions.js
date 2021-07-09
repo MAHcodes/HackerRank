ebe18b09ff7a24ec281061fa04aa69964c5d9e6f
mhmdali102 <mhmdali102@bk.ru>
2021-07-09 12:36:18 +0300

    js day 0 & 1|

---
+++ Javascript/10DaysOfJavaScript/functions.js
@@ -0,0 +1,32 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+    let fac = 1;
+
+    function factorial(num) {
+        for (let i=1; i<=num; i++) {
+           fac *= i; 
+        }
+        return fac;
+    }
+
